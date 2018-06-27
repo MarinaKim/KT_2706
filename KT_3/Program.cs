92da4bffc03e58cf7067fe4c2f21b6730ff30bce
КимМ <@227_14>
2018-06-27 12:02:46 +0600

    Add project files.

---
+++ KT_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using KT_3.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Human h = new Human();
+            var t = p.Brain;
+            p.month = 12;
+            p.FullName = "Ivanov Ivan";
+            Console.WriteLine(p.ShortName);
+        }
+    }
+}
---
+++ KT_3/model/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT_3.model
+{
+    public class Person : Human
+    {
+        readonly int mp = 56; // поле чтения, почти как константа, ее возможно изменить в конструкторе
+        public Person()
+        {
+            mp = 89;
+            age = 23;
+        }
+
+        public DateTime dob { get; set; }
+        public int age { get; } // авто свойство , ограничили на изменение данного св-ва, поместить значение можно в конструкторе
+        //private int iin; //переменная
+        public int month;
+        private string FullName_;
+       
+        public string FullName {
+            get
+            {
+                return FullName_;
+            }
+            set
+            {
+                FullName_ = value;
+                ShortName = string.Format( "{0} {1}.",value.Substring(0, value.IndexOf(' ')),value.Substring(value.IndexOf(' ')+1,1));
+            }
+        }
+
+        public string ShortName { get; private set; }
+
+        private int iin_;
+        private int iin
+        {
+            get {return iin_; }
+            set
+            {
+                if (value < 0)
+                    iin_ = 0;
+                else
+                    iin_ = value;                 
+            }
+        }
+        public void HaveBrain()
+        {
+            Console.WriteLine(Brain);
+        }
+    }
+
+    public class Human
+    {
+       public bool Brain { get; protected set; }
+    }
+}
